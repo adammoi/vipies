184a13879c40767e34cabdd157a5e18d4745cdfa
adammoi <111665453+adammoi@users.noreply.github.com>
2022-10-10 00:24:54 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,6 +7,18 @@ if [ "$(systemd-detect-virt)" == "openvz" ]; then
 		echo "OpenVZ is not supported"
 		exit 1
 fi
+
+localip=$(hostname -I | cut -d\  -f1)
+hst=( `hostname` )
+dart=$(cat /etc/hosts | grep -w `hostname` | awk '{print $2}')
+if [[ "$hst" != "$dart" ]]; then
+echo "$localip $(hostname)" >> /etc/hosts
+fi
+
+mkdir -p /etc/xray
+touch /etc/xray/domain
+touch /etc/xray/scdomain
+
 # ==========================================
 # Color
 RED='\033[0;31m'
@@ -18,6 +30,52 @@ PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
+
+secs_to_human() {
+    echo "Installation time : $(( ${1} / 3600 )) hours $(( (${1} / 60) % 60 )) minute's $(( ${1} % 60 )) seconds"
+}
+start=$(date +%s)
+ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
+sysctl -w net.ipv6.conf.all.disable_ipv6=1 >/dev/null 2>&1
+sysctl -w net.ipv6.conf.default.disable_ipv6=1 >/dev/null 2>&1
+
+coreselect=''
+cat> /root/.profile << END
+# ~/.profile: executed by Bourne-compatible login shells.
+if [ "$BASH" ]; then
+  if [ -f ~/.bashrc ]; then
+    . ~/.bashrc
+  fi
+fi
+mesg n || true
+clear
+END
+chmod 644 /root/.profile
+
+echo -e "[ ${GREEN}INFO${NC} ] Preparing the install file"
+apt install git curl -y >/dev/null 2>&1
+apt install python -y >/dev/null 2>&1
+echo -e "[ ${GREEN}INFO${NC} ] Aight good ... installation file is ready"
+
+mkdir /var/lib/SIJA;
+echo "IP=" >> /var/lib/SIJA/ipvps.conf
+
+echo "Add Domain for vmess/vless/trojan dll"
+echo " "
+read -rp "Input ur domain : " -e pp
+    if [ -z $pp ]; then
+        echo -e "
+        Nothing input for domain!
+        Then a random domain will be created"
+    else
+        echo "$pp" > /root/scdomain
+	echo "$pp" > /etc/xray/scdomain
+	echo "$pp" > /etc/xray/domain
+	echo $pp > /root/domain
+        echo "IP=$pp" > /var/lib/SIJA/ipvps.conf
+    fi
+
+
 # Link Hosting Kalian Untuk Ssh Vpn
 SSH="raw.githubusercontent.com/adammoi/vipies/main/ssh"
 # Link Hosting Kalian Untuk Xray
@@ -30,17 +88,11 @@ MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
 IZIN=$(wget -qO- ipinfo.io/ip);
 
-rm -f setup.sh
-clear
-if [ -f "/etc/xray/domain" ]; then
-echo "Script Already Installed"
-exit 0
-fi
+
 mkdir /var/lib/SIJA;
 echo "IP=" >> /var/lib/SIJA/ipvps.conf
 wget https://${SIJA}/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
 wget https://${XRAY}/ins-xray.sh && chmod +x ins-xray.sh && screen -S xray ./ins-xray.sh
-wget https://${SIJA}/slhost.sh && chmod +x slhost.sh && ./slhost.sh
 # Websocket
 wget https://${websocket}/edu.sh && chmod +x edu.sh && ./edu.sh
 #install xray grpc
@@ -50,24 +102,8 @@ rm -f /root/ssh-vpn.sh
 rm -f /root/ins-xray.sh
 rm -f /root/edu.sh
 rm -f /root/sl-grpc.sh
-cat <<EOF> /etc/systemd/system/autosett.service
-[Unit]
-Description=autosetting
-Documentation=github.com/admmoi
 
-[Service]
-Type=oneshot
-ExecStart=/bin/bash /etc/set.sh
-RemainAfterExit=yes
 
-[Install]
-WantedBy=multi-user.target
-EOF
-systemctl daemon-reload
-systemctl enable autosett
-wget -O /etc/set.sh "https://${SIJA}/set.sh"
-chmod +x /etc/set.sh
-history -c
 echo "1.2" > /home/ver
 echo " "
 echo "Installation has been completed!!"echo " "
@@ -106,7 +142,6 @@ echo "   - Autoreboot On 05.00 GMT +7" | tee -a log-install.txt
 echo "   - Autobackup Data" | tee -a log-install.txt
 echo "   - Restore Data" | tee -a log-install.txt
 echo "   - Auto Delete Expired Account" | tee -a log-install.txt
-echo "   - Full Orders For Various Services" | tee -a log-install.txt
 echo "   - White Label" | tee -a log-install.txt
 echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
 rm -f setup.sh
