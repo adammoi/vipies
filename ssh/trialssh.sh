ca358e71935c63da7fc4fd258930db4d35453fae
adammoi <111665453+adammoi@users.noreply.github.com>
2022-11-04 22:17:07 +0700

    Update trialssh.sh

--- ssh/trialssh.sh
+++ ssh/trialssh.sh
@@ -31,46 +31,27 @@ fi
 clear
 IP=$(wget -qO- ipinfo.io/ip);
 ssl="$(cat ~/log-install.txt | grep -w "Stunnel5" | cut -d: -f2)"
-sqd="$(cat ~/log-install.txt | grep -w "Squid" | cut -d: -f2)"
-ovpn="$(netstat -nlpt | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
-ovpn2="$(netstat -nlpu | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
 Login=trial`</dev/urandom tr -dc X-Z0-9 | head -c4`
 hari="1"
 Pass=1
 clear
 systemctl restart ws-tls
 systemctl restart ws-nontls
-#systemctl restart ssh-ohp
-#systemctl restart dropbear-ohp
-#systemctl restart openvpn-ohp
 useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
 exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
 hariini=`date -d "0 days" +"%Y-%m-%d"`
 expi=`date -d "$masaaktif days" +"%Y-%m-%d"`
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
 echo -e "================================"
-echo -e "Informasi Trial SSH & OpenVPN"
+echo -e " Trial Account Details "
 echo -e "================================"
 echo -e "IP/Host            : $IP"
 echo -e "Domain             : $domain"
 echo -e "Username           : $Login"
 echo -e "Password           : $Pass"
-#echo -e "OpenSSH            : 443, 22"
-#echo -e "Dropbear           : 443, 109, 143"
-echo -e "SSL/TLS            :$ssl"
-echo -e "Port Squid         :$sqd"
-#echo -e "OHP SSH            : 8181"
-#echo -e "OHP Dropbear       : 8282"
-#echo -e "OHP OpenVPN        : 8383"
+echo -e "SSL/TLS            : $ssl"
 echo -e "SSH Websocket SSL  : 443"
 echo -e "SSH Websocket HTTP : 8880"
-echo -e "OVPN Websocket     : 2086"
-echo -e "OVPN Port TCP      : $ovpn"
-echo -e "OVPN Port UDP      : $ovpn2"
-echo -e "OVPN Port SSL      : 990"
-echo -e "OVPN TCP           : http://$IP:89/tcp.ovpn"
-echo -e "OVPN UDP           : http://$IP:89/udp.ovpn"
-echo -e "OVPN SSL           : http://$IP:89/ssl.ovpn"
 echo -e "BadVpn             : 7100-7200-7300"
 echo -e "Created            : $hariini"
 echo -e "Expired            : $expi"
