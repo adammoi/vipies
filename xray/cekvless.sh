942859aa7900a0fb9b3f78988ee3c2c8a5399386
adammoi <111665453+adammoi@users.noreply.github.com>
2022-12-25 02:03:25 +0700

    Update cekvless.sh

--- xray/cekvless.sh
+++ xray/cekvless.sh
@@ -1,65 +1,48 @@
 #!/bin/bash
-# My Telegram : https://t.me/Akbar218
-# ==========================================
-# Color
-RED='\033[0;31m'
-NC='\033[0m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHT='\033[0;37m'
-# ==========================================
-# Getting
-MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Checking VPS"
-IZIN=$( curl ipinfo.io/ip | grep $MYIP )
-if [ $MYIP = $MYIP ]; then
-echo -e "${NC}${GREEN}Permission Accepted...${NC}"
-else
-echo -e "${NC}${RED}Permission Denied!${NC}";
-echo -e "${NC}${LIGHT}Fuck You!!"
-exit 0
-fi
-clear
-echo -n > /tmp/other.txt
-data=( `cat /etc/xray/config.json | grep '^#&' | cut -d ' ' -f 2 | sort | uniq`);
+clear 
+echo -n > /tmp/other.txt 
+data=(`cat /etc/xray/config.json | grep '#& ' | cut -d ' ' -f 2 | sort | uniq`) ; 
+echo -e "┌─────────────────────────────────────────────────┐" 
+echo -e "│              • VLESS USER ONLINE •              │"
+echo -e "└─────────────────────────────────────────────────┘" 
+echo -e "┌─────────────────────────────────────────────────┐" 
 
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[44;1;39m       ⇱ Vless User Login ⇲        \E[0m"
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+for akun in "${data[@]}" 
+do
+if [[ -z "$akun" ]] ; then 
+account="none"
+fi
+echo -n > /tmp/ipvless.txt 
+data2=(`cat /var/log/xray/access.log | tail -n 500 | cut -d " " -f 3 | sed 's/tcp://g' | cut -d ":" -f 1 | sort | uniq`) ; 
+for ip in "${data2[@]}" 
+do
 
-for akun in "${data[@]}"
-do
-if [[ -z "$akun" ]]; then
-akun="tidakada"
-fi
-echo -n > /tmp/ipvless.txt
-data2=( `netstat -anp | grep ESTABLISHED | grep tcp6 | grep xray | awk '{print $5}' | cut -d: -f1 | sort | uniq`);
-for ip in "${data2[@]}"
-do
-jum=$(cat /var/log/xray/access.log | grep -w $akun | awk '{print $3}' | cut -d: -f1 | grep -w $ip | sort | uniq)
-if [[ "$jum" = "$ip" ]]; then
-echo "$jum" >> /tmp/ipvless.txt
+jum=$(cat /var/log/xray/access.log | grep -w "$akun" | tail -n 500 | cut -d " " -f 3 | sed 's/tcp://g' | cut -d ":" -f 1 | grep -w "$ip" | sort | uniq) 
+if [[ "$jum" = "$ip" ]] ; then 
+echo "$jum" >> /tmp/ipvless.txt 
 else
-echo "$ip" >> /tmp/other.txt
+echo "$ip" >> /tmp/other.txt 
 fi
-jum2=$(cat /tmp/ipvless.txt)
-sed -i "/$jum2/d" /tmp/other.txt > /dev/null 2>&1
-done
-jum=$(cat /tmp/ipvless.txt)
-if [[ -z "$jum" ]]; then
-echo > /dev/null
-else
-jum2=$(cat /tmp/ipvless.txt | nl)
-echo "user : $akun";
-echo "$jum2";
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-fi
-rm -rf /tmp/ipvmess.txt
-done
-oth=$(cat /tmp/other.txt | sort | uniq | nl)
-echo "$oth";
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-rm -rf /tmp/other.txt
+jum2=$(cat /tmp/ipvless.txt ) 
+sed -i "/$jum2/d" /tmp/other.txt > /dev/null  2>&1 
+done 
+
+day=$(cat /tmp/ipvless.txt ) 
+if [[ -z "$jum" ]] ;  then 
+echo > /dev/null 
+else 
+jum2=$(cat /tmp/ipvless.txt | nl ) 
+echo -e "│     user :  $akun                               |" ; 
+echo -e "│     $jum2                                       |" ; 
+fi 
+rm -rf /tmp/ipvless.txt 
+done 
+
+rm -rf /tmp/other.txt 
+echo -e "└─────────────────────────────────────────────────┘"  
+echo -e "┌────────────────────── BY ───────────────────────┐" 
+echo -e "│                  • ADAM SIJA •                  │"
+echo -e "└─────────────────────────────────────────────────┘"  
+echo  " " 
+read  -n 1 -s -r -p  "    Press any key to back on menu " 
+m-vless
