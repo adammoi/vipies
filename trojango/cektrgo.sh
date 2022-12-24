406f0ddd09926af385ac8286031ced929f116a1c
adammoi <111665453+adammoi@users.noreply.github.com>
2022-12-25 02:08:40 +0700

    Update cektrgo.sh

--- trojango/cektrgo.sh
+++ trojango/cektrgo.sh
@@ -1,66 +1,47 @@
 #!/bin/bash
-# SL
-# ==========================================
-# Color
-RED='\033[0;31m'
-NC='\033[0m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHT='\033[0;37m'
-# ==========================================
-# Getting
-MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Checking VPS"
-IZIN=$( curl ipinfo.io/ip | grep $MYIP )
-if [ $MYIP = $MYIP ]; then
-echo -e "${NC}${GREEN}Permission Accepted...${NC}"
-else
-echo -e "${NC}${RED}Permission Denied!${NC}";
-echo -e "${NC}${LIGHT}Fuck You!!"
-exit 0
-fi
-clear
-echo -n > /tmp/other.txt
-data=( `cat /etc/trojan-go/akun.conf | grep '^###' | cut -d ' ' -f 2`);
-echo "------------------------------------";
-echo "-----=[ Trojan-Go User Login ]=-----";
-echo "------------------------------------";
-for akun in "${data[@]}"
+clear 
+echo -n > /tmp/other.txt 
+data=(`cat /etc/xray/config.json | grep '^###' | cut -d ' ' -f 2 | sort | uniq`) ; 
+echo -e "┌─────────────────────────────────────────────────┐" 
+echo -e "│              • TROJANGO USER ONLINE •             │"
+echo -e "└─────────────────────────────────────────────────┘" 
+echo -e "┌─────────────────────────────────────────────────┐" 
+for akun in "${data[@]}" 
 do
-if [[ -z "$akun" ]]; then
-akun="tidakada"
+if [[ -z "$akun" ]] ; then 
+account="none"
 fi
-echo -n > /tmp/iptrojango.txt
-data2=( `netstat -anp | grep ESTABLISHED | grep tcp6 | grep trojan-go | awk '{print $5}' | cut -d: -f1 | sort | uniq`);
-for ip in "${data2[@]}"
+echo -n > /tmp/iptrojango.txt 
+data2=(`cat /etc/trojan-go/akun.conf | tail -n 500 | cut -d " " -f 3 | sed 's/tcp://g' | cut -d ":" -f 1 | sort | uniq`) ; 
+for ip in "${data2[@]}" 
 do
-jum=$(cat /var/log/trojan-go/trojan-go.log | grep -w $akun | awk '{print $3}' | cut -d: -f1 | grep -w $ip | sort | uniq)
-if [[ "$jum" = "$ip" ]]; then
-echo "$jum" >> /tmp/iptrojango.txt
-else
-echo "$ip" >> /tmp/other.txt
-fi
-jum2=$(cat /tmp/iptrojango.txt)
-sed -i "/$jum2/d" /tmp/other.txt > /dev/null 2>&1
-done
-jum=$(cat /tmp/iptrojango.txt)
-if [[ -z "$jum" ]]; then
-echo > /dev/null
-else
-jum2=$(cat /tmp/iptrojango.txt | nl)
-echo "user : $akun";
-echo "$jum2";
-echo "------------------------------------";
-fi
-rm -rf /tmp/iptrojango.txt
-done
-oth=$(cat /tmp/other.txt | sort | uniq | nl)
-echo "other";
-echo "$oth";
-echo "------------------------------------";
-echo "Script Mod By ADAM SIJA"
-rm -rf /tmp/other.txt
 
+jum=$(cat /etc/trojan-go/akun.conf | grep -w "$akun" | tail -n 500 | cut -d " " -f 3 | sed 's/tcp://g' | cut -d ":" -f 1 | grep -w "$ip" | sort | uniq) 
+if [[ "$jum" = "$ip" ]] ; then 
+echo "$jum" >> /tmp/iptrojango.txt 
+else
+echo "$ip" >> /tmp/other.txt 
+fi
+jum2=$(cat /tmp/iptrojango.txt ) 
+sed -i "/$jum2/d" /tmp/other.txt > /dev/null  2>&1 
+done 
+
+day=$(cat /tmp/iptrojango.txt ) 
+if [[ -z "$jum" ]] ;  then 
+echo > /dev/null 
+else 
+jum2=$(cat /tmp/iptrojango.txt | nl ) 
+echo -e "│     user :  $akun                               |" ; 
+echo -e "│     $jum2                                       |" ; 
+fi 
+rm -rf /tmp/iptrojango.txt 
+done 
+
+rm -rf /tmp/other.txt 
+echo -e "└─────────────────────────────────────────────────┘"  
+echo -e "┌────────────────────── BY ───────────────────────┐" 
+echo -e "│                  • ADAM SIJA •                  │"
+echo -e "└─────────────────────────────────────────────────┘"  
+echo ""
+read -n 1 -s -r -p "   Press any key to back on menu"
+m-trojango
